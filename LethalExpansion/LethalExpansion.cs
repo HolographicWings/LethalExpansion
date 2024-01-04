6677db91b9edd74600a356ff08677bc4b08a63ac
DENIEL-VALERO Ewan <ewandenielvalero@orange.fr>
2024-01-05 00:44:36 +0100

    1.3.14

--- LethalExpansion/LethalExpansion.cs
+++ LethalExpansion/LethalExpansion.cs
@@ -43,7 +43,7 @@ namespace LethalExpansion
     {
         private const string PluginGUID = "LethalExpansion";
         private const string PluginName = "LethalExpansion";
-        private const string VersionString = "1.3.13";
+        private const string VersionString = "1.3.14";
         public static readonly Version ModVersion = new Version(VersionString);
         /*private readonly Version[] CompatibleModVersions = {
             new Version(1, 3, 11)
